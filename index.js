fe8e7889b23e17cf06180350b87a46f0524e7dad
dxy159 <richard.ni.wchs@gmail.com>
2016-12-27 18:53:48 -0700

    Hello World4

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
         res.send(req.query['hub.challenge'])
     }
-    res.send('887863829')
+    res.send('Error, wrong token')
 })
 
 // Spin up the server
