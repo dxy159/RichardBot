148b4132c6cca646742fff86a82b2807f2d1d6f3
dxy159 <richard.ni.wchs@gmail.com>
2016-12-30 23:53:45 -0700

    undo again

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const request = require('request')
 const app = express()
 const weather = require('./weather/weather_api_request.js')
 const messages = require('./messages.js')
-const res = require('./responses.js')
+const r = require('./responses.js')
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -59,7 +59,7 @@ app.post('/webhook/', function (req, res) {
             })
         } else {
             // messages.sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-            messages.sendTextMessage(sender, res.handleInput(text))
+            messages.sendTextMessage(sender, r.handleInput(text))
         }
       }
       if (event.postback) {
