f9bb241b6c6b1f3be7cfd62d6e34d1a6f87ea05a
dxy159 <richard.ni.wchs@gmail.com>
2016-12-30 21:31:38 -0700

    attempt 2 to fix desription

--- weather/weather_api_request.js
+++ weather/weather_api_request.js
@@ -21,8 +21,8 @@ module.exports = function accessWeather(city, callback) {
 		response.on('end', function() {
 
 			var json = JSON.parse(body)
-			var temperature = parseInt(json["main"][0]["temp"] - 273)
-			var description = json["weather"]["description"]
+			var temperature = parseInt(json["main"]["temp"] - 273)
+			var description = json["weather"][0]["description"]
 			var icon = json["weather"]["icon"]
 			var iconURL = "http://openweathermap.org/img/w/" + icon + ".png"
 			callback(temperature, description, iconURL)
