19420674114ed8d0fcba9397355b2e40bcae888c
dxy159 <richard.ni.wchs@gmail.com>
2017-01-03 20:39:28 -0500

    doing stats now because games doesn't work

--- nba/nba_api_request.js
+++ nba/nba_api_request.js
@@ -1,14 +1,37 @@
 const request = require('request')
 const cheerio = require('cheerio')
 
-const url = 'https://watch.nba.com'
+module.exports.games = function(callback) {
+	const url = 'https://watch.nba.com'
 
-request(url, function(err, response, body) {
-	if (!err && response.statusCode === 200) {
-		$ = cheerio.load(body)	
+	request(url, function(err, response, body) {
+		if (!err && response.statusCode === 200) {
+			$ = cheerio.load(body)	
 
-		$("schedule-list").each(function(game) {
-			console.log(game)
-		})
-	}
+			var num_games = $("div.schedule-list > div").length
+			console.log(num_games)
+		}
+	})
+}
+
+module.exports.stats = function(callback) {
+	const url = 'http://www.espn.com/nba/statistics'
+
+	request(url, function(err, response, body) {
+		if (!err && response.statusCode == 200) {
+			$ = cheerio.load(body)
+
+			var $categories = $("#my-players-table")
+
+			// POINTS 
+			var $points = $("#my-players-table nth-child(1) nth-child(1)")
+			callback($points)
+			// var $westbrook = $($points + "tbody nth:child(2) a").text()
+			// callback($westbrook)
+		}
+	})
+}
+
+module.exports.stats(function(westbrook) {
+	console.log(westbrook)
 })
