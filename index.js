461b0d71b3e8eb3df3ecc4baeb67ee895bac51f7
dxy159 <richard.ni.wchs@gmail.com>
2017-01-30 18:39:36 -0500

    added separate file to handle nba

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 const weather = require('./weather/weather_api_request.js')
 const messages = require('./messages.js')
 const r = require('./responses.js')
-const nba = require('./nba/nba_api_request.js')
+const nba = require('./nba/handle_nba.js')
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -54,59 +54,9 @@ app.post('/webhook/', function (req, res) {
             continue
         }
         if (r.editText(text).indexOf("NBA") >= 0) {
-            if (r.editText(text).indexOf("NBASTATS") >= 0) {
-                if (r.editText(text).indexOf("POINT") >= 0 || r.editText(text).indexOf("PT") >= 0) {
-                    nba.stats("POINTS", function(msg) {
-                        messages.nba_stats_back(sender, "Points per game: Season Leaders\n" + msg)
-                    })
-                } else if (r.editText(text).indexOf("ASSIST") >= 0) {
-                    nba.stats("ASSISTS", function(msg) {
-                        messages.nba_stats_back(sender, "Assists per game: Season Leaders\n" + msg)
-                    }) 
-                } else if (r.editText(text).indexOf("FIELDGOAL") >= 0 || r.editText(text).indexOf("FG") >= 0) {
-                    nba.stats("FG", function(msg) {
-                        messages.nba_stats_back(sender, "Field goal % per game: Season Leaders\n" + msg)
-                    })
-                } else if (r.editText(text).indexOf("REBOUND") >= 0 || r.editText(text).indexOf("REB") >= 0) {
-                    nba.stats("REBOUNDS", function(msg) {
-                        messages.nba_stats_back(sender, "Rebounds per game: Season Leaders\n" + msg)
-                    })
-                } else if (r.editText(text).indexOf("BLOCK") >= 0 || r.editText(text).indexOf("BLK") >= 0) {
-                    nba.stats("BLOCKS", function(msg) {
-                        messages.nba_stats_back(sender, "Blocks per game: Season Leaders\n" + msg)
-                    })
-                } else if (r.editText(text).indexOf("STEAL") >= 0 || r.editText(text).indexOf("STL") >= 0) {
-                    nba.stats("STEALS", function(msg) {
-                        messages.nba_stats_back(sender, "Steals per game: Season Leaders\n" + msg)
-                    })
-                } else {
-                    let msg = "Please choose one of the following categories and I will tell you the top 5 season leaders."
-                    messages.nba_stats(sender, msg)
-                }
-                continue
-            } else if (r.editText(text).indexOf("NBAGAMES") >= 0) {
-                nba.games(function(msg, num_games) {
-                    messages.nba_games(sender, "Here are the games for today!\n\n" + msg, num_games)
-                })
-            } else if (r.editText(text).indexOf("NBASTANDINGS") >= 0) {
-                if (r.editText(text).indexOf("EAST") >= 0) {
-                    nba.standings("EASTERN", function(msg) {
-                        messages.nba_standings_back(sender, "Current Standing in the Eastern Conference:\n\n" + msg)
-                    })
-                } else if (r.editText(text).indexOf("WEST") >= 0) {
-                    nba.standings("WESTERN", function(msg) {
-                        messages.nba_standings_back(sender, "Current Standing in the Western Conference:\n\n" + msg)
-                    })
-                } else {
-                    messages.nba_standings(sender, "Select a Conference!")
-                }
-            } else {
-                let msg = "RichardBot can provide you with all different sorts of information about the NBA. Please be kept updated as more and more cool NBA features will be added soon!"
-                messages.nba(sender, msg)
-            }
+            nba.handle_nba(sender, text)
             continue
-        }
-        else if (r.editText(text) === "WEATHER") {
+        } else if (r.editText(text) === "WEATHER") {
             let weatherDescription = "Welcome to RichardBot's awesome and handy weather feature. Type in 'Weather' followed by any city name and I will provide you with that location's weather information! For example, 'Weather Calgary' will allow me to give you Calgary's current weather information. For now sending your location will not work due to a technical issue, but I will keep you updated if this problem is resolved!" 
             messages.location_quick_replies(sender, weatherDescription)
             continue
@@ -141,5 +91,4 @@ app.post('/webhook/', function (req, res) {
     }
     res.sendStatus(200)
   })
-
 
---
+++ nba/handle_nba.js
@@ -0,0 +1,58 @@
+const messages = require('./../messages.js')
+const r = require('./../responses.js')
+const nba = require('./nba_api_request.js')
+
+function handle_nba(sender, text) {
+    if (r.editText(text).indexOf("NBASTATS") >= 0) {
+        if (r.editText(text).indexOf("POINT") >= 0 || r.editText(text).indexOf("PT") >= 0) {
+            nba.stats("POINTS", function(msg) {
+                messages.nba_stats_back(sender, "Points per game: Season Leaders\n" + msg)
+            })
+        } else if (r.editText(text).indexOf("ASSIST") >= 0) {
+            nba.stats("ASSISTS", function(msg) {
+                messages.nba_stats_back(sender, "Assists per game: Season Leaders\n" + msg)
+            }) 
+        } else if (r.editText(text).indexOf("FIELDGOAL") >= 0 || r.editText(text).indexOf("FG") >= 0) {
+            nba.stats("FG", function(msg) {
+                messages.nba_stats_back(sender, "Field goal % per game: Season Leaders\n" + msg)
+            })
+        } else if (r.editText(text).indexOf("REBOUND") >= 0 || r.editText(text).indexOf("REB") >= 0) {
+            nba.stats("REBOUNDS", function(msg) {
+                messages.nba_stats_back(sender, "Rebounds per game: Season Leaders\n" + msg)
+            })
+        } else if (r.editText(text).indexOf("BLOCK") >= 0 || r.editText(text).indexOf("BLK") >= 0) {
+            nba.stats("BLOCKS", function(msg) {
+                messages.nba_stats_back(sender, "Blocks per game: Season Leaders\n" + msg)
+            })
+        } else if (r.editText(text).indexOf("STEAL") >= 0 || r.editText(text).indexOf("STL") >= 0) {
+            nba.stats("STEALS", function(msg) {
+                messages.nba_stats_back(sender, "Steals per game: Season Leaders\n" + msg)
+            })
+        } else {
+            let msg = "Please choose one of the following categories and I will tell you the top 5 season leaders."
+            messages.nba_stats(sender, msg)
+        }
+    } else if (r.editText(text).indexOf("NBAGAMES") >= 0) {
+        nba.games(function(msg, num_games) {
+            messages.nba_games(sender, "Here are the games for today!\n\n" + msg, num_games)
+        })
+    } else if (r.editText(text).indexOf("NBASTANDINGS") >= 0) {
+        if (r.editText(text).indexOf("EAST") >= 0) {
+            nba.standings("EASTERN", function(msg) {
+                messages.nba_standings_back(sender, "Current Standing in the Eastern Conference:\n\n" + msg)
+            })
+        } else if (r.editText(text).indexOf("WEST") >= 0) {
+            nba.standings("WESTERN", function(msg) {
+                messages.nba_standings_back(sender, "Current Standing in the Western Conference:\n\n" + msg)
+            })
+        } else {
+            messages.nba_standings(sender, "Select a Conference!")
+        }
+    } else {
+        let msg = "RichardBot can provide you with all different sorts of information about the NBA. Please be kept updated as more and more cool NBA features will be added soon!"
+        messages.nba(sender, msg)
+    }
+    
+
+}
+
