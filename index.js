2bb951aa7ffb6aa4e31e739c1db14148286ccd10
dxy159 <richard.ni.wchs@gmail.com>
2017-01-30 19:29:06 -0500

    test5

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.post('/webhook/', function (req, res) {
             var status = JSON.stringify(event.message.quick_reply.payload)
             text = status
         }
-        messages.sendTextMessage(sender, text)
+        //messages.sendTextMessage(sender, text)
         if (text === 'Generic') {
             messages.sendGenericMessage(sender)
             continue
